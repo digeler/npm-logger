13e35308b70e979b2b04c801c02cea44dbf71f56
System Administrator <root@FLIX-6-7910.europe.corp.microsoft.com>
2019-08-23 16:57:48 +0300

    changed with kubectl

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,12 @@ FROM ubuntu:19.10
 ARG NPM_BUILD_DIR
 
 # Install dependencies.
-RUN apt-get update
+RUN apt-get update && apt-get install curl -y 
 RUN apt-get install -y iptables
 RUN apt-get install -y ipset
 WORKDIR /usr/bin
+RUN curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+RUN chmod +x ./kubectl
 COPY test.sh /usr/bin/test.sh
 RUN chmod +x /usr/bin/test.sh 
 CMD [ "test.sh" ]
--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+while [ ! -f /mnt/npmlogs/config  ] ;
+do
+      sleep 2
+      echo "waiting for the config file ,please place it in share"
+done
 
  sleep 5
 echo "start log is :" >> /mnt/npmlogs/$HOSTNAME.log
@@ -19,12 +23,26 @@ echo "*********Dumping IPTABLES-SAVE************" >>/mnt/npmlogs/$HOSTNAME.log
 echo ""
 iptables-save >> /mnt/npmlogs/$HOSTNAME.log
 echo done *************************** >> /mnt/npmlogs/$HOSTNAME.log
+echo "****************Dumping kubectl network policy all name-spaces***********" >>/mnt/npmlogs/$HOSTNAME.log
+kubectl get networkpolicy --all-namespaces --kubeconfig /mnt/npmlogs/config >>/mnt/npmlogs/$HOSTNAME.log
+kubectl get networkpolicy --all-namespaces -o yaml --kubeconfig /mnt/npmlogs/config >>/mnt/npmlogs/$HOSTNAME.log
+echo done *************************** >> /mnt/npmlogs/$HOSTNAME.log
+echo ""
+echo "****************Dumping kubectl get pods -o wide ***********" >>/mnt/npmlogs/$HOSTNAME.log
+kubectl get pods --all-namespaces -o wide --kubeconfig /mnt/npmlogs/config >>/mnt/npmlogs/$HOSTNAME.log
+kubectl get pods --all-namespaces -o yaml --kubeconfig /mnt/npmlogs/config >>/mnt/npmlogs/$HOSTNAME.log
+echo done *************************** >> /mnt/npmlogs/$HOSTNAME.log
+echo "****************Dumping kubectl cluster-info ***********" >>/mnt/npmlogs/$HOSTNAME.log
+kubectl cluster-info --kubeconfig /mnt/npmlogs/config >>/mnt/npmlogs/$HOSTNAME.log
+echo done *************************** >> /mnt/npmlogs/$HOSTNAME.log
+echo ""
 echo Closing log at >> /mnt/npmlogs/$HOSTNAME.log
 date >> /mnt/npmlogs/$HOSTNAME.log
 echo "Please share this logs with Support" >> /mnt/npmlogs/$HOSTNAME.log
+
 while $(true)
 do 
-echo "completed collecting logs"
+echo "completed collecting logs,do not delete the logger till you copied the files"
 sleep 40
 done
 
